ee0ea90d159fb3fab827218523a91f7a2bb61607
Alisson Diogo <38612359+AlissonDiogo@users.noreply.github.com>
2021-11-05 23:06:09 -0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { signInWithEmailAndPassword } from "@firebase/auth";
 
-const firebaseConfig = {
+const firebaseConfig = { 
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
